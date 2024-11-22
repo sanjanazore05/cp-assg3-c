7db85fa0ff210be798143735735542bc756a79dc
sanjanazore05 <zoresanjana50@gmail.com>
2024-11-22 23:49:05 +0530

    Update and rename main to main.c

---
+++ sanjana.c/main.c
@@ -0,0 +1,29 @@
+/*
+ Roll No.:57
+ Student Name: Sanjana Zore
+ Program Title:WAP to Calculate the area of square.
+Task Given By:
+ -Roll NO.of Assignee:60
+ -Assignee Name:Sumaiya Shaikh
+ */
+ 
+ #include<stdio.h>
+ int main() {
+ int side,area;
+ printf("\nEnter the length of side:");
+ scanf("%d",&side);
+ area=side*side;
+ printf("\nArea of square:%d",area);
+ 
+return 0;
+ 
+
+ }
+ 
+ /*
+ OUTPUT
+ 
+ Enter the length of side:4
+ 
+ Area of square:16
+ */
